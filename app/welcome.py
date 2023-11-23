27460753580699c216bbc5a42ea9c911bb7bf25f
arnrxn <arnfried.rixen@gmail.com>
2023-11-23 13:48:22 +0100

    Add welcome page

---
+++ app/welcome.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title("Welcome to the universal calculator")
+    st.subheader("A simple tool to perform daily calculations")
+
+
+if __name__ == "__main__":
+    main()
